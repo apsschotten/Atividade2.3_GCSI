f548a60ca1de3f7a38633d359bc6d37923630c5b
Hnr1qwe <henriquetrachtafilho@gmail.com>
2025-10-01 14:41:47 -0400

    Bug-fix: Function "dividir".

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ function multiplicar() {
 };
 
 function dividir() {
-    let valorX = document.getElementById('valorX_piv').value;
+    let valorX = document.getElementById('valorX_div').value;
     let valorY = document.getElementById('valorY_div').value;
     let resultadoDivisão = valorX / valorY;
     document.getElementById('resultado_div').value = resultadoDivisão;
