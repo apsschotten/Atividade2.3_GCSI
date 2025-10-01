11852869dbd74c3d7a54bc120bdb03e61d87af33
Hnr1qwe <henriquetrachtafilho@gmail.com>
2025-10-01 14:40:28 -0400

    Create function "dividir".

--- script.js
+++ script.js
@@ -18,3 +18,10 @@ function multiplicar() {
     let resultadoMultiplicação = valorX * valorY;
     document.getElementById('resultado_mult').value = resultadoMultiplicação;
 };
+
+function dividir() {
+    let valorX = document.getElementById('valorX_piv').value;
+    let valorY = document.getElementById('valorY_div').value;
+    let resultadoDivisão = valorX / valorY;
+    document.getElementById('resultado_div').value = resultadoDivisão;
+};
